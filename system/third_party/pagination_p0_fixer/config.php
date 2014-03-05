ba80697c79a4503c909e5b7b2a24cf0c04a4163a
John Morton <john@johnfmorton.com>
2014-03-05 12:10:44 -0500

    Initial Commit

---
+++ system/third_party/pagination_p0_fixer/config.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+* EE Pagination P0 Fixer Config file
+*
+* @package			Pagination P0 Fixer-addon
+* @version			1.0.0
+* @author			John F Morton <john@johnfmorton.com>
+* @link				https://github.com/johnfmorton/EE-Pagination-P0-Fixer
+* @license			http://opensource.org/licenses/MIT
+*/
+
+if ( ! defined('PAGINATION_P0_FIXER_NAME')) {
+	define('PAGINATION_P0_FIXER_NAME', 'EE Pagination P0 Fixer');
+	define('PAGINATION_P0_FIXER_PACKAGE', 'Pagination P0 Fixer');
+	define('PAGINATION_P0_FIXER_VERSION', '1.0.0');
+	define('PAGINATION_P0_FIXER_DOCS', 'https://github.com/johnfmorton/EE-Pagination-P0-Fixer');
+}
+
+$config['name'] = PAGINATION_P0_FIXER_NAME;
+$config['version'] = PAGINATION_P0_FIXER_VERSION;
+$config['docs'] = PAGINATION_P0_FIXER_DOCS;
+
+
+/*
+
+The MIT License (MIT)
+
+Copyright (c) [year] [fullname]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
